2fd7895bc779495f70281641274a70e94a2a16a7
Praneethachodey <praneethachodey@gmail.com>
2020-09-28 01:22:30 +0530

    [Praneetha]Added: welcome message

---
+++ src/com/bridgeLabz/practice/address/AddressBookMain.java
@@ -0,0 +1,8 @@
+package com.bridgeLabz.practice.address;
+
+public class AddressBookMain {
+   public static void main(String[] args) {
+	   System.out.println("Welcome to Address Book Problem");
+	   
+       }
+}
